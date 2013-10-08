aa1b0f438c46b8626a1f60aa8cb9194c48049bb1
Bretto <separ8@gmail.com>
2013-10-08 13:54:43 +1100

    add import export

--- js/controllers.js
+++ js/controllers.js
@@ -6,5 +6,190 @@ var controllers = angular.module('App.controllers', []);
 controllers.controller('AppCtrl', function ($scope, $rootScope, $timeout, $log, $http, DataModel) {
     $log.log('AppCtrl');
 
+
+    // desing issue do we want properties or arrays
+    var data = [
+        {
+            name: 'brett',
+            children: [
+                {
+                    test: 'ok'
+                }
+            ]
+        }
+    ];
+
+
+    function toNodeList(data) {
+
+        function toArray(data) {
+            var a = [];
+            angular.forEach(data, function (obj) {
+                a.push(toKeyValue(obj));
+            })
+
+            return a;
+        }
+
+
+        function toKeyValue(obj) {
+
+            var a = [];
+
+            for (var key in obj) {
+                if (angular.isArray(obj[key])) {
+                    a.push({key: key, value: toArray(obj[key])});
+                } else {
+                    a.push({key: key, value: obj[key]});
+                }
+            }
+
+            return a;
+        }
+
+        var test = toArray(data);
+
+        return test;
+    }
+
+    function toJson(nodes) {
+
+
+//        function iterArr(arr) {
+//
+//            var a = [];
+//
+//            angular.forEach(arr, function (obj) {
+//                var d = {};
+//
+//                if (angular.isArray(obj)) {
+//                    iterArr(obj);
+//                } else {
+//
+//                    if (angular.isArray(obj.value)){
+//                        d[obj.key] = iterArr(obj.value);
+//                    }else{
+//                        d[obj.key] = obj.value;
+//                    }
+//                    a.push(d);
+//                }
+//            });
+//
+//            return a;
+//        }
+////
+//        var arr = iterArr(nodes);
+//        var test = JSON.stringify(angular.copy(arr));
+////        var x = eval(test);
+//        return test;
+
+
+        function toArray(data, obj) {
+            var a = [];
+            var o = obj || {};
+
+
+            angular.forEach(data, function (item) {
+                console.log(JSON.stringify(item));
+                if (angular.isArray(item)) {
+                    //toArray(item, o);
+                    var res = toObject2(item, o)
+                    a.push(res);
+                } else {
+                    var res = toObject(item, o)
+                    a.push(res);
+                }
+            })
+
+            return a;
+        }
+
+
+        function toObject2(arr) {
+            var o = {};
+            angular.forEach(arr, function (obj) {
+
+                if (angular.isArray(obj.value)) {
+                    o[obj.key] = toArray(obj.value)
+                } else {
+                    o[obj.key] = obj.value;
+                }
+            });
+
+            return o;
+        }
+
+
+        function toObject(obj, o) {
+
+            if (angular.isArray(obj.value)) {
+
+                var res = toArray(obj.value, o);
+                o[obj.key] = res;
+
+                return o;
+            } else {
+                o[obj.key] = obj.value;
+                return o;
+            }
+
+//            for (var key in obj) {
+//                if (angular.isArray(obj[key])) {
+//                    o[obj.key] = toArray(obj.value);
+//                } else {
+//                    o[obj.key] = obj.value;
+//                }
+//                a.push(o);
+//            }
+
+        }
+
+        var test = toArray(nodes);
+
+        return JSON.stringify(test);
+
+    }
+
+
+    var test = toNodeList(data);
+//        $log.info(test);
+
+    $scope.nodes = test;
+
+
+
+//        $scope.nodes = [
+//            [
+//                {key: 'name', value: 'brett'},
+//                {key: 'children', value: [
+//                    {key: 'name', value: 'zoe'}
+//                ]
+//                }
+//            ]
+//        ]
+
+//        $scope.nodes = [
+//            [
+//                {key: 'name', value: 'brett'},
+//                {key: 'children', value: [
+//                    {key: 'name', value: 'zoe'}
+//                ]
+//                }
+//            ],
+//
+//            [
+//                {key: 'name', value: 'brett'},
+//                {key: 'children', value: [
+//                    {key: 'name', value: 'zoe'}
+//                ]
+//                }
+//            ]
+//        ]
+
+    $scope.doIt = function(){
+        var test = toNodeList(eval(toJson(angular.copy($scope.nodes))));
+        $scope.json = test;
+    }
+
 });
 
--- js/directives.js
+++ js/directives.js
@@ -3,66 +3,149 @@
 var directives = angular.module('App.directives', []);
 
 
+directives.directive('nodeList', function ($log, $compile) {
 
-directives.directive('nodeEditor', function ($log) {
+    function link(scope, element, attrs) {
+        $log.info('nodeList')
 
-    function link($scope, element, attrs) {
-        $log.info('nodeEditor')
+        scope.$watch(function () {
+            return scope.nodes
+        }, function () {
+            scope.toJSON(scope.nodes);
+        }, true);
+
+        scope.toJSON = function(nodes){
+//
+            var arr = [];
+            angular.forEach(nodes, function(node){
+                var d = {};
+                //d[data.key] = data.value;
+                arr.push(d)
+            })
+
+            return 'ok';
+        }
     }
+
+
 
     return {
         restrict: 'A',
         link: link,
-        scope: {},
-        templateUrl: 'partials/node-editor.html',
-        controller: function($scope, $element) {
-            $scope.nodes = [{}];
-            $scope.json = '';
+        scope: {nodes:'='},
+        templateUrl: 'partials/node-list.html'
+//        controller: function($scope, $element) {
+//
+//        }
+    };
+});
 
-            $scope.$watch(function(){return $scope.nodes}, function(){
-                renderJson($scope.nodes);
-            },true)
 
-            this.newNode = function(node) {
-                $log.info('newNode');
-                var data = {};
-                $scope.nodes.push(data);
+directives.directive('nodeItem', function ($log, $compile) {
 
-            }
-
-            function renderJson(nodes){
-
-                var arr = [];
-                angular.forEach(nodes, function(node){
-                    var data = {};
-                    data[node.key] = node.value;
-                    arr.push(data)
+    function link(scope, element, attrs) {
+        $log.info('nodeItem');
+        scope.toJSON = function(){
+            var arr = [];
+                angular.forEach(node, function(data){
+                    var d = {};
+                    d[data.key] = data.value;
+                    arr.push(d)
                 })
+            return JSON.stringify(arr);
+        }
+    }
 
-                $scope.json = JSON.stringify(arr);
+
+    return {
+        restrict: 'A',
+        link: link,
+//        scope: {node:'='},
+        templateUrl: 'partials/node-item.html',
+        controller: function ($scope, $element) {
+//            $scope.node = [{}];
+//            $scope.json = '';
+
+//            $scope.$watch(function(){return $scope.node}, function(){
+//                renderJson($scope.node);
+//            },true)
+
+            this.newData = function (data) {
+                $log.info('newData 1');
+                var d = data || {};
+                $scope.node.push(d);
             }
+//
+            this.deleteData = function (data) {
+                $log.info('deleteData');
+                var i = $scope.node.indexOf(data);
+                if (i > 0)$scope.node.splice(i, 1);
+            }
+
+
+
+//            function renderJson(node){
+//
+//                var arr = [];
+//                angular.forEach(node, function(data){
+//                    var d = {};
+//                    d[data.key] = data.value;
+//                    arr.push(d)
+//                })
+//
+//                $scope.json = JSON.stringify(arr);
+//                $scope.children = $scope.node;
+//            }
         }
     };
 });
 
 
-directives.directive('nodeData', function ($log) {
+directives.directive('nodeData', function ($log, $compile) {
 
     function link(scope, element, attrs, ctrl) {
         $log.info('nodeData')
 
-        scope.newNode = function(){
-            ctrl.newNode(scope.node);
+
+        var data = scope.data();
+//        if(angular.isArray(data.value)){
+//            data.value = data.value[0];
+//        }
+
+        scope.data = data;
+
+        scope.newData = function () {
+//            $log.info('newData3s');
+            ctrl.newData();
+        }
+
+        scope.deleteData = function () {
+            ctrl.deleteData(scope.data);
+        }
+
+        if (scope.data.key === 'children') {
+            $log.info('children')
+
+//            var newElement = angular.element('<div node-item></div>');
+            var newElement = angular.element("<div node-list nodes='nodes'></div>");
+
+            var newScope = scope.$new();
+            newScope.nodes = scope.data.value;
+            $compile(newElement)(newScope, function (clonedElement, scope) {
+                //attach the clone to DOM document at the right place
+//                console.log(clonedElement, scope);
+                element.find('.value').replaceWith(clonedElement);
+            });
         }
 
     }
 
     return {
-        require: '^nodeEditor',
+        require: '^nodeItem',
         restrict: 'A',
+        scope: {data: '&'},
         templateUrl: 'partials/node-data.html',
         link: link
-//        scope: {key:'=', value:'='}
     };
 });
 
@@ -71,10 +154,9 @@ directives.directive('ngEnter', function ($log) {
     return function (scope, element, attrs) {
         element.bind("keydown keypress", function (event) {
 
-            $log.info('keydown');
-            if(event.which === 13) {
+            if (event.which === 13) {
 
-                scope.$apply(function (){
+                scope.$apply(function () {
                     scope.$eval(attrs.ngEnter);
                 });
 
@@ -84,14 +166,33 @@ directives.directive('ngEnter', function ($log) {
     };
 });
 
-directives.directive('autoFocus', function($timeout) {
+directives.directive('ngDelete', function ($log) {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+
+            if (event.which === 8) {
+
+                if (scope.data.key === undefined || scope.data.key.length === 0) {
+
+                    scope.$apply(function () {
+                        scope.$eval(attrs.ngDelete);
+                    });
+                    event.preventDefault();
+                }
+
+            }
+        });
+    };
+});
+
+directives.directive('autoFocus', function ($timeout) {
     return {
-        link: function ( scope, element, attrs ) {
-            scope.$watch( attrs.autoFocus, function () {
-                $timeout( function () {
+        link: function (scope, element, attrs) {
+            scope.$watch(attrs.autoFocus, function () {
+                $timeout(function () {
                     element[0].focus();
                     scope.$digest();
-                },0);
+                }, 0);
             }, true);
         }
     };
